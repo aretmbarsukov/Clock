5b04506b89d550c2389ac27eab5c486985870324
aretmbarsukov <karasbobra@gmail.com>
2025-07-06 11:45:13 +0200

    2_clock

--- clock.js
+++ clock.js
@@ -1,8 +1,17 @@
-function updateClock() {
+function updateClocks() {
   const now = new Date();
-  const time = now.toLocaleTimeString();
-  document.getElementById("clock").textContent = time;
+
+  // Локальний час (Іспанія)
+  const esTime = now.toLocaleTimeString("es-ES", { hour12: false });
+  document.getElementById("clock-es").textContent = `Іспанія: ${esTime}`;
+
+  // Український час (за таймзоною Europe/Kyiv)
+  const uaTime = now.toLocaleTimeString("uk-UA", {
+    hour12: false,
+    timeZone: "Europe/Kyiv",
+  });
+  document.getElementById("clock-ua").textContent = `Україна: ${uaTime}`;
 }
 
-setInterval(updateClock, 1000);
-updateClock();
+setInterval(updateClocks, 1000);
+updateClocks();
